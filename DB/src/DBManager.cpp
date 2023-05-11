5e3a90eb693769a62769992d9d005ee6aea0de05
AnastasiaDolotova <ad4019809@gmail.com>
2023-05-11 21:40:05 +0300

    remove some

--- DB/src/DBManager.cpp
+++
@@ -1,7 +0,0 @@
-#include "DBManager.hpp"
-
-DBManager::DBManager(){}
-
-int DBManager::connect(const std::string& dsn){}
-
-std::vector<std::vector<std::string>> DBManager::exec(const std::string& sqlQuery){}
