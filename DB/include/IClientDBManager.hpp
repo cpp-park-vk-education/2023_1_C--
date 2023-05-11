b6297048ef7b0e063b99a5c33d62df9d2334d024
AnastasiaDolotova <ad4019809@gmail.com>
2023-05-11 21:40:05 +0300

    Interfaces

--- DB/include/IClientDBManager.hpp
+++ DB/include/IClientDBManager.hpp
@@ -1,19 +1,21 @@
 #pragma once
 
-#include <string>
-
-struct Client{};
+#include "Client.hpp"
 
 class IClientDBManager {
 public:
-    virtual Client getClient(const std::string& login) = 0;
+    virtual Client getClient(const QString& login) = 0;
 
-    virtual int changeLogin(const std::string& old_login, const std::string& new_login) = 0;
+    virtual Client createClient(const QString&, const QString&, const QString&) = 0;
 
-    virtual int changePassword(const std::string& login, const std::string& new_password) = 0;
+    virtual Client changeLogin(const QString& old_login, const QString& new_login) = 0;
 
-    virtual int setFirstname(const std::string& login, const std::string& firstname) = 0;
+    virtual Client changePassword(const QString& login, const QString& new_password) = 0;
 
-    virtual int setLastname(const std::string& login, const std::string& lastname) = 0;
+    virtual Client setFirstname(const QString& login, const QString& firstname) = 0;
+
+    virtual Client setLastname(const QString& login, const QString& lastname) = 0;
+
+    virtual QList<Client> getClientsInRoom(const int roomID) = 0;
 
 };
