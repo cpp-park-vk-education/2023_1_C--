43911fb15bc263a7ed6cb3e038fb2e0f856eeb55
Oleg <osperelygin@gmail.com>
2023-05-14 23:55:30 +0300

    Added main page

--- client/Room/include/IMainPageUi.hpp
+++ client/Room/include/IMainPageUi.hpp
@@ -5,7 +5,7 @@
 class IMainPageUi {
 public:
     virtual ~IMainPageUi() {}
-    virtual void ShowMainPage(const std::vector<RoomData>& rooms) = 0;
+    virtual void ShowMainPage(std::vector<RoomData> rooms) = 0;
 };
 
 using IMainPageUiSPtr = std::shared_ptr<IMainPageUi>;
--- client/src/MainPage.cpp
+++ client/src/MainPage.cpp
@@ -1,11 +1,19 @@
 #include "MainPage.hpp"
 #include "ui_MainPage.h"
+#include <algorithm>
+#include <optional>
+#include <QListWidget>
+#include <QAbstractButton>
+
+const int ROOM_ID_ROLE = 1;
 
 MainPage::MainPage(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MainPage)
 {
     ui->setupUi(this);
+    connect(ui->listWidget, &QListWidget::itemDoubleClicked, this, &MainPage::SelectRoom);
+    connect(ui->selectButton, &QAbstractButton::clicked, this, &MainPage::OnSelectButtonClicked);
 }
 
 MainPage::~MainPage()
@@ -13,4 +21,34 @@ MainPage::~MainPage()
     delete ui;
 }
 
-void MainPage::ShowMainPage(const std::vector<RoomData>& rooms) {}
+std::optional<RoomData> MainPage::FindRoom(const int roomId) {
+    for (auto it = rooms_.begin(); it !=rooms_.end(); ++it)
+        if (it->info.id == roomId)
+             return *it;
+    return std::nullopt;
+}
+
+void MainPage::ShowMainPage(std::vector<RoomData> rooms) {
+    rooms_ = std::move(rooms);
+    for_each(rooms_.begin(), rooms_.end(), [this](const RoomData& room) {
+        QListWidgetItem* roomWidget = new QListWidgetItem(ui->listWidget);
+        roomWidget->setText(QString::fromStdString(room.info.name));
+        roomWidget->setData(ROOM_ID_ROLE, room.info.id);
+        ui->listWidget->addItem(roomWidget);
+    });
+}
+
+void MainPage::SelectRoom(QListWidgetItem *item) {
+    int roomId = item->data(ROOM_ID_ROLE).toInt();
+    auto roomData = FindRoom(roomId);
+    qDebug() << "Room was found successfully";
+    // roomSwitcher_->ShowRoom(roomData.value());
+}
+
+void MainPage::OnSelectButtonClicked() {
+    QListWidgetItem* item = ui->listWidget->currentItem();
+    int roomId = item->data(ROOM_ID_ROLE).toInt();
+    auto roomData = FindRoom(roomId);
+    qDebug() << "Room was found successfully";
+    // roomSwitcher_->ShowRoom(roomData.value());
+}
