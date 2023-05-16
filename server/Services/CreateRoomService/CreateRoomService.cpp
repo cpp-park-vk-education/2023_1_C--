9e8be18a719ef9c0a73c66915aad5e6cb25993f5
Kononykhin Maksim <kononelder@gmail.com>
2023-05-16 06:24:52 +0300

    remove debug

--- server/Services/CreateRoomService/CreateRoomService.cpp
+++ server/Services/CreateRoomService/CreateRoomService.cpp
@@ -14,13 +14,10 @@ void CreateRoomService::CreateRoomWithUsers(const std::vector<std::string>& logi
 {
     auto it = logins.begin();
 
-    qDebug() << logins.size();
-
     auto room = roomManager->insertRoom(QString::fromStdString(*it++), QString::fromStdString(roomName));
 
     for (it; it != logins.end(); ++it)
     {
-        qDebug() << QString::fromStdString(*it);
         roomManager->addClientToRoom(room.ID, QString::fromStdString(*it));
     }
 }
