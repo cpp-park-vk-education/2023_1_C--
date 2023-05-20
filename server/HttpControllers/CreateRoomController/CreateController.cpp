40ef89d4924dff1e89a18d71662878535c0d0c84
Kononykhin Maksim <kononelder@gmail.com>
2023-05-20 20:07:56 +0300

    create room

--- server/HttpControllers/CreateRoomController/CreateController.cpp
+++ server/HttpControllers/CreateRoomController/CreateController.cpp
@@ -31,5 +31,40 @@ void CreateRoomController::service(IHttpRequest* request, IHttpResponse* respons
     for (const auto& login : logins)
         stdLogins.push_back(login.toString().toStdString());
 
-    createService->CreateRoomWithUsers(stdLogins, roomName.toStdString());
+    auto room = createService->CreateRoomWithUsers(stdLogins, roomName.toStdString());
+
+    auto clients = createService->getClientsByLogin(stdLogins);
+
+    QJsonObject responseJSONObject;
+
+    fillJsonClients(responseJSONObject, clients);
+
+    QJsonObject roomInfo;
+
+    roomInfo.insert("RoomInfo", QJsonValue({
+        qMakePair(QString::fromStdString("ID"), QJsonValue(room.ID)),
+        qMakePair(QString::fromStdString("Name"), QJsonValue(room.name))
+        }));
+
+    responseJSONObject.insert("Room", roomInfo);
+
+    response->setBody(QJsonDocument(responseJSONObject).toJson(QJsonDocument::Compact).toStdString());
+
+}
+
+void CreateRoomController::fillJsonClients(QJsonObject& json, const std::vector<Client>& clients)
+{
+    QJsonArray members;
+
+    for (const auto& member : clients)
+        members.append(
+            QJsonObject({
+                qMakePair(QString::fromStdString("Login"), QJsonValue(member.login)),
+                qMakePair(QString::fromStdString("Username"), QJsonValue(member.username)),
+                qMakePair(QString::fromStdString("Firstname"), QJsonValue(member.firstName)),
+                qMakePair(QString::fromStdString("Lastname"), QJsonValue(member.lastName))
+            })
+        );
+
+    json.insert("Members", members);
 }
--- server/Services/CreateRoomService/CreateRoomService.hpp
+++ server/Services/CreateRoomService/CreateRoomService.hpp
@@ -13,9 +13,13 @@ public:
     CreateRoomService(std::shared_ptr<IClientDBManager> clientManager, std::shared_ptr<IRoomDBManager> roomManager)
         : clientManager(clientManager), roomManager(roomManager){}
 
-    void CreateRoomWithUsers(const std::vector<std::string>& logins, const std::string roomName);
+    Room CreateRoomWithUsers(const std::vector<std::string>& logins, const std::string roomName);
 
     bool isClientExists(const std::string& login);
+
+    std::vector<Client> getClientsByLogin(const std::vector<std::string>&);
+    
+
 private:
     std::shared_ptr<IClientDBManager> clientManager;
 
